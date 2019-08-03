dc1768abd25590fc806a687ed6f441c30909baa3
Simrin <REQUIRED>
2019-08-03 14:17:14 -0500

    table w select, paginate, order

--- web/templates/carmenta/src/App.js
+++ web/templates/carmenta/src/App.js
@@ -2,6 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StartScrapingDialog from './components/StartScrapingDialog'
+import CandidateItem from './components/CandidateItem'
+
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
 
 function App() {
   return (
@@ -12,6 +26,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <StartScrapingDialog />
+        <CandidateItem user={rows} />
       </header>
     </div>
   );
