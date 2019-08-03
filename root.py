9dee7151690d5e6ab988b70785e1833c00cea65d
Simrin <REQUIRED>
2019-08-02 21:01:23 -0500

    init for react template

--- root.py
+++ root.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask import Blueprint, render_template, abort
 from api.api import api_page
+from templates import app
 
-app = Flask(__name__)
+#Load this config object for development mode
+app.config.from_object('configurations.DevelopmentConfig')
 app.register_blueprint(api_page, url_prefix='/api')
 
 @app.route('/')
 def show():
     return 'Hello, World!'
 
+
+app.run()
