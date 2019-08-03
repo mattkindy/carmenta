586f7df5eaa25b4c0f1a69f648f413f3bc54b2e2
Matt Kindy <matthew.kindy@praetorian.com>
2019-08-03 02:01:34 -0500

    Working company scraping for Praetorian

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
 
 from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from parsel import Selector
@@ -9,8 +10,11 @@ from selenium.webdriver.common.keys import Keys
 import urllib
 import json
 
+from scraper.interface.candidate import Candidate
+from scraper.interface.company import Company
+
 api_page = Blueprint('api', __name__,
-                        template_folder='templates')
+                     template_folder='templates')
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -18,88 +22,89 @@ chrome_options.add_argument("--headless")
 
 @api_page.route('/')
 def show(page):
-    try:
-        return render_template('pages/%s.html' % page)
-    except TemplateNotFound:
-        abort(404)
+  try:
+    return render_template('pages/%s.html' % page)
+  except TemplateNotFound:
+    abort(404)
+
 
 @api_page.route('/scrape_user')
 def scrape_user():
-    link = request.args.get('link')
+  link = request.args.get('link')
 
-    #TODO use other library to get a specific profile
-    return link
+  # TODO use other library to get a specific profile
+  return link
+
 
 def linkedin_login(driver):
-    linkedin_username = 'anna.pobletts+hackathon2@praetorian.com'
-    linkedin_password = 'Welcome2PS!'
-    driver.get('https://www.linkedin.com/login')
+  linkedin_username = 'matt.kindy.ii@gmail.com'
+  linkedin_password = '6M0tpDQ5U2He'
 
-    username = driver.find_element_by_id('username')
-    username.send_keys(linkedin_username)
+  driver.get('https://www.linkedin.com/login')
 
-    password = driver.find_element_by_id('password')
-    password.send_keys(linkedin_password)
+  username = driver.find_element_by_id('username')
+  username.send_keys(linkedin_username)
 
-    sign_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
-    sign_in_button.click()
+  password = driver.find_element_by_id('password')
+  password.send_keys(linkedin_password)
+
+  sign_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
+  sign_in_button.click()
+
 
 @api_page.route('/scraper')
 def scraper():
-    keyword = "Praetorian"
-    if request.args.get('keyword'):
-        keyword = request.args.get('keyword')
-    search_number = 2
-    if request.args.get('num'):
-        search_number = request.args.get('num')
+  return scrape()
 
-    search_query = 'site:linkedin.com/in/ AND "' + keyword + '"'
 
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    linkedin_login(driver)
-    driver.get('https://www.google.com/search?' + urllib.parse.urlencode({'q':search_query, "num":search_number}))
-    linkedin_urls = driver.find_elements_by_class_name('iUh30')
-    linkedin_urls = [url.text for url in linkedin_urls]
+def scrape():
+  keyword = "Security"
+  driver = webdriver.Chrome()
+  linkedin_login(driver)
+  company_url = 'https://www.linkedin.com/company/praetorian/'
+  praetorian = Company(company_url, driver=driver, scrape=False)
+  print('Making request for {}'.format(company_url))
+  employees = praetorian.get_employees()
 
-    entries = []
-    for linkedin_url in linkedin_urls:
-        entry = {}
-        driver.get(linkedin_url)
-        sel = Selector(text=driver.page_source)
+  existing_users = set()
 
-        # xpath to extract the text from the class containing the name
-        name = sel.xpath('//*[starts-with(@class,  "inline t-24 t-black t-normal break-words")]/text()').extract_first()
+  employees = [
+    employee
+    for employee in employees
+    if 'search' not in employee.linkedin_url
+       and employee.name not in existing_users
+       and employee.experiences
+  ]
 
-        if name:
-            entry['name'] = name.strip()
+  security_employees = [
+    employee
+    for employee in employees
+    if keyword.lower() in employee.experiences[0].position_title.lower()
+  ]
 
-        # xpath to extract the text from the class containing the job title
-        job_title = sel.xpath('//*[starts-with(@class, "mt1 t-18 t-black t-normal")]/text()').extract_first()
+  entries = []
 
-        if job_title:
-            entry['job_title'] = job_title.strip()
+  for employee in security_employees:
+    linkedin_url = employee.linkedin_url
+    position_title = employee.experiences[0].position_title
 
-        # xpath to extract the text from the class containing the company
-        company = sel.xpath(
-            '//*[starts-with(@class, "text-align-left ml2 t-14 t-black t-bold full-width lt-line-clamp lt-line-clamp--multi-line ember-view")]/text()').extract_first()
+    print('Scraping: {} for {}: {}'.format(linkedin_url, employee.name, position_title))
+    candidate = Candidate(linkedin_url, driver=driver, scrape=False)
+    candidate.scrape(close_on_complete=False)
 
-        if company:
-            entry['company'] = company.strip()
+    experience = candidate.experiences[0] if candidate.experiences else None
+    education = candidate.educations[0] if candidate.educations else None
 
-        # xpath to extract the text from the class containing the college
-        college = sel.xpath(
-            '//*[starts-with(@class, "text-align-left ml2 t-14 t-black t-bold full-width lt-line-clamp lt-line-clamp--multi-line ember-view")]/text()').extract_first()
+    entries.append({
+      'name': candidate.name,
+      'job_title': experience.position_title if experience else None,
+      'company': experience.institution_name if experience else None,
+      'college': education.institution_name if education else None,
+      'location': experience.location if experience else None,
+    })
 
-        if college:
-            entry['college'] = college.strip()
+  return json.dumps(entries)
 
-        # xpath to extract the text from the class containing the location
-        location = sel.xpath('//*[starts-with(@class, "t-16 t-black t-normal inline-block")]/text()').extract_first()
 
-        if location:
-            entry['location'] = location.strip()
-
-        entries.append(entry)
-        linkedin_url = driver.current_url
-
-    return json.dumps(entries)
+if __name__ == '__main__':
+  scrape()
