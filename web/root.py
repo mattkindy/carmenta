f0f84fd87596abda447051eda25ce277fc19acef
Matt Kindy <matthew.kindy@praetorian.com>
2019-08-03 10:42:46 -0500

    Making flask/react stuff work with docker setup

--- web/root.py
+++ web/root.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from api.api import api_root
 from api.scrape import scrape_root
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./templates/public', template_folder='./templates/static')
 app.register_blueprint(api_root)
 app.register_blueprint(scrape_root)
 
-
 @app.route('/')
 def show():
-  return 'Hello, World!'
+  return render_template('index.html')
 
 
 if __name__ == '__main__':
--- web/templates/public/bundle.js
+++ web/templates/public/bundle.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/Users/sguglani/Desktop/hello_template/templates/public";
+/******/ 	__webpack_require__.p = "/Users/mkindy/carmenta/web/templates/public";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
